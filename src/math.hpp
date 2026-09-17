065c9aa1204c63bdf5fc2d77b0f7ed0eeb08415c
Ben Vaughan <b.f.vaughan2026@gmail.com>
2026-09-16 23:56:49 -0400

    feat: added constructor for mat4 to init all values

--- src/math.hpp
+++ src/math.hpp
@@ -2,6 +2,13 @@
 #define RENDERER_MATH_HPP
 
 class mat4 {
+    public:
+        mat4(float x);
+        mat4(float x1, float x2, float x3, float x4,
+            float x5, float x6, float x7, float x8, 
+            float x9, float x10, float x11, float x12, 
+            float x13, float x14, float x15, float x16);
+        
     private:
         float data[16] = {0};
 };
